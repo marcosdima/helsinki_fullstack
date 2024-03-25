f29cfdf44c4d9795166e1a0bae3cac238c15ee8a
marcosdima <marcosdimatteo.dev@gmail.com>
2024-03-25 12:21:09 -0300

    Exercise 2.7: Agenda Step 2

--- part2/agenda/src/App.jsx
+++ part2/agenda/src/App.jsx
@@ -10,6 +10,11 @@ const App = () => {
   const addName = (event) => {
     event.preventDefault()
     const name = { name: newName }
+
+    const nameExists = persons.find(person => person.name === newName)
+
+    if (nameExists) return alert(`Name '${newName}' already exists...`)
+
     setPersons(persons.concat(name))
     setNewName("")
   }
