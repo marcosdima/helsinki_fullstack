1cda658dfe0491246a2a38654d15d91579a1043e
marcosdima <marcosdimatteo.dev@gmail.com>
2024-04-01 09:30:41 -0300

    Exercise 3.17

--- part3/agenda_backend/src/index.js
+++ part3/agenda_backend/src/index.js
@@ -12,6 +12,7 @@ app.use(express.static('dist'))
 app.use(cors())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :res-content'))
 
+// Add a person to the agenda...
 app.post('/api/persons', (request, response) => {
   const data = request.body
 
@@ -30,12 +31,14 @@ app.post('/api/persons', (request, response) => {
   person.save().then(res => response.json(res))
 })
 
+// Get all...
 app.get('/api/persons', (request, response) => {
   Person.find({}).then(res => {
     response.json(res)
   })
 })
 
+// Get by id...
 app.get('/api/persons/:id', (request, response, next) => {
   const id = request.params.id
   Person.findById(id)
@@ -46,11 +49,28 @@ app.get('/api/persons/:id', (request, response, next) => {
     .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
+// Delete...
+app.delete('/api/persons/:id', (request, response, next) => {
   const id = request.params.id
   Person.findByIdAndDelete(id)
     .then(result => {
       response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+// Update...
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
     })
     .catch(error => next(error))
 })
