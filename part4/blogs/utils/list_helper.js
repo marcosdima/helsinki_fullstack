a62cb5540e75bc9c54f3d4dc651aabd3c21b0e53
marcosdima <marcosdimatteo.dev@gmail.com>
2024-04-01 15:18:28 -0300

    Exercise 4.4

--- part4/blogs/utils/list_helper.js
+++ part4/blogs/utils/list_helper.js
@@ -1,6 +1,15 @@
 // eslint-disable-next-line no-unused-vars
 const dummy = (blogs) => 1
 
+const totalLikes = (blogs) => {
+	const reducer = (sum, item) => sum + item.likes
+
+	return blogs.length === 0
+		? 0
+		: blogs.reduce(reducer, 0)
+}
+
 module.exports = {
-	dummy
+	dummy,
+	totalLikes
 }
