3f3cc5de11daa7d57be3a678a0626ca6817a7bcc
marcosdima <marcosdimatteo.dev@gmail.com>
2024-04-04 11:28:37 -0300

    Importing bloglist-frontend

---
+++ part5/bloglist-frontend/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState, useEffect } from 'react'
+import Blog from './components/Blog'
+import Login from './components/Login'
+import blogService from './services/blogs'
+
+const App = () => {
+  const [blogs, setBlogs] = useState([])
+
+  useEffect(() => {
+    blogService.getAll().then(blogs =>
+      setBlogs( blogs )
+    )  
+  }, [])
+
+  return (
+    <div>
+      {
+        user === null
+          ? <Login />
+          : <Blogs blogs={blogs} />
+      }
+    </div>
+  )
+}
+
+export default App
