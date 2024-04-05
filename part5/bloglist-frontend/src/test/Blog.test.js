1e17a32532788b3f94e30463ad92a2fd863849c2
marcosdima <marcosdimatteo.dev@gmail.com>
2024-04-05 19:25:20 -0300

    Exercise 5.14

--- part5/bloglist-frontend/src/test/Blog.test.js
+++ part5/bloglist-frontend/src/test/Blog.test.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from '../components/Blog'
 import helper from './test_helper'
 
@@ -16,5 +17,13 @@ describe('Blog Component Test', () => {
     test('and nothing else.', () => {
         const body_of_blog = container.querySelector('.toggle-on')
         expect(body_of_blog).toHaveStyle('display: none')
+    })
+    test('unless you click DA button...', async () => {
+        const user = userEvent.setup()
+        const DA_button = screen.getByText('view')
+        const body_of_blog = container.querySelector('.toggle-on')
+        await user.click(DA_button)
+
+        expect(body_of_blog).not.toHaveStyle('display: none')
     })
 })
