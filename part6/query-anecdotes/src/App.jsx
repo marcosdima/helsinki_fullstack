12451beda5d7d58bf3447401921e699838c3a15c
marcosdima <marcosdimatteo.dev@gmail.com>
2024-04-09 12:30:12 -0300

    Exercise 6.20

--- part6/query-anecdotes/src/App.jsx
+++ part6/query-anecdotes/src/App.jsx
@@ -1,5 +1,7 @@
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
+import { useQuery } from '@tanstack/react-query'
+import { getAnecdotes } from './request'
 
 const App = () => {
 
@@ -7,13 +9,18 @@ const App = () => {
     console.log('vote')
   }
 
-  const anecdotes = [
-    {
-      "content": "If it hurts, do it more often",
-      "id": "47145",
-      "votes": 0
-    },
-  ]
+  const result = useQuery({
+      queryKey: ['anecdotes'],
+      queryFn: getAnecdotes,
+      retry: 1
+  })
+
+  if (result.isLoading) 
+    return <div>loading data...</div>
+  if (result.isError) 
+    return <div>anecdote service not available due to problems in server</div>
+
+  const anecdotes = result.data
 
   return (
     <div>
