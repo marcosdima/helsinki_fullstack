bc73bfe313e83c7ff6941ac3a6e03ee564454bce
marcosdima <marcosdimatteo.dev@gmail.com>
2024-04-16 10:39:30 -0300

    Exercise 8.3

--- part8/library/index.js
+++ part8/library/index.js
@@ -102,10 +102,18 @@ const typeDefs = `
     genres: [String!]!
   }
 
+  type Author {
+    name: String!,
+    born: Int,
+    bookCount: Int,
+    id: ID!
+  }
+
   type Query {
     bookCount: Int!,
     authorCount: Int!,
-    allBooks: [Book!]!
+    allBooks: [Book!]!,
+    allAuthors: [Author!]!
   }
 `
 
@@ -113,7 +121,16 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books
+    allBooks: () => books,
+    allAuthors: () => authors
+  },
+  Author: {
+    bookCount: (root) => 
+      books
+        .reduce((previus, current) => 
+          current.author === root.name
+          ? previus += 1 : previus,
+        0)
   }
 }
 
