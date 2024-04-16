01e6c489a3f6ba19d373df128ce0b90947b151b5
marcosdima <marcosdimatteo.dev@gmail.com>
2024-04-16 15:31:00 -0300

    Exercise 8.6

--- part8/library/index.js
+++ part8/library/index.js
@@ -1,5 +1,6 @@
 const { ApolloServer } = require('@apollo/server')
 const { startStandaloneServer } = require('@apollo/server/standalone')
+const { v1: uuid } = require('uuid')
 
 let authors = [
   {
@@ -97,7 +98,7 @@ const typeDefs = `
   type Book {
     title: String!,
     published: Int!,
-    author: String,
+    author: String!,
     id: ID!,
     genres: [String!]!
   }
@@ -114,6 +115,15 @@ const typeDefs = `
     authorCount: Int!,
     allBooks(author: String, genre: String): [Book!]!,
     allAuthors : [Author!]!
+  }
+
+  type Mutation {
+    addBook(
+      title: String!,
+      published: Int!,
+      author: String!,
+      genres: [String!]!
+    ): Book
   }
 `
 
@@ -136,6 +146,18 @@ const resolvers = {
           current.author === root.name
           ? previus += 1 : previus,
         0)
+  },
+  Mutation: {
+    addBook: (root, args) => {
+      if (!authors.find(author => author.name === args.author)) {
+        const author = { name: args.author, id: uuid()}
+        authors = authors.concat(author)
+      }
+      
+      const book = { ...args, id: uuid() }
+      books = books.concat(book)
+      return book
+    }
   }
 }
 
