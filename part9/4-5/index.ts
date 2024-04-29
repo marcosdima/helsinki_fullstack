e1c745715e9f39f5f93c4b4e805f3c8bac27d18f
marcosdima <marcosdimatteo.dev@gmail.com>
2024-04-29 10:35:33 -0300

    Exercise 9.5

--- part9/4-5/index.ts
+++ part9/4-5/index.ts
@@ -1,9 +1,26 @@
 import express from 'express';
+import calculateBmi from './bmiCalculator'
 
 const app = express()
 
-app.get('/hello', (_re1, res) => {
+app.get('/hello', (_req, res) => {
     res.send('Hello Full Stack!');
+})
+
+app.get('/bmi', (req, res) => {
+  const height = Number(req.query.height);
+  const weight = Number(req.query.weight);
+  
+  // Check if the parameters are right...
+  if (isNaN(height) || isNaN(weight)) res.send({
+    error: "malformatted parameters"
+  })
+
+  res.json({
+    height,
+    weight,
+    bmi: calculateBmi({ height, mass: weight })
+  })
 })
 
 const PORT = 3003;
