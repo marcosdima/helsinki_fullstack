347a7b05cd569a114e5d59f953bd76aded38b5eb
marcosdima <marcosdimatteo.dev@gmail.com>
2024-05-07 13:36:46 -0300

    Exercise 9.10

--- part9/patientor-backend/src/index.ts
+++ part9/patientor-backend/src/index.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import diaryRouter from './routes/diaries';
+import diagnosesRouter from './routes/diagnoses';
 import cors from 'cors';
 
 const app = express();
@@ -12,6 +14,9 @@ app.get('/api/ping', (_req, res) => {
     console.log('someone pinged here');
     res.send('pong');
 });
+
+app.use('/api/diaries', diaryRouter);
+app.use('/api/diagnoses', diagnosesRouter);
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
